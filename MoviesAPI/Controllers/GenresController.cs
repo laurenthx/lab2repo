ed52f476697f6dbebd057e3009b4d338761d209a
laurenthx <93344572+laurenthx@users.noreply.github.com>
2024-02-13 23:44:37 +0100

    repo2-commit

--- MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/Controllers/GenresController.cs
@@ -1,53 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MoviesAPI.Entities;
+using MoviesAPI.Filters;
 using MoviesAPI.Services;
 
 namespace MoviesAPI.Controllers
 {
     [Route("api/genres")]
     [ApiController]
-    public class GenresController: ControllerBase
+
+    public class GenresController : ControllerBase
     {
 
         private readonly IRepository repository;
+        private readonly ILogger<GenresController> logger;
 
-        public GenresController(IRepository repository)
+        public GenresController(IRepository repository, ILogger<GenresController> logger)
         {
             this.repository = repository;
+            this.logger = logger;
         }
         [HttpGet]//api/genres
         [HttpGet("list")]//api/genres/list
         [HttpGet("/allgenres")]//allgenres
+        //[ResponseCache(Duration = 60)]
+        [ServiceFilter(typeof(MyActionFilter))]
         public async Task<ActionResult<List<Genre>>> Get()
         {
+            logger.LogInformation("Getting all the genres");
             return await repository.GetAllGenres();
         }
 
-        [HttpGet("{Id:int}")]//api/genres/example
-        public ActionResult<Genre> Get(int Id,string param2)
+        [HttpGet("{Id:int}", Name ="getGenre")]//api/genres/example
+        public ActionResult<Genre> Get(int Id, string param2)
         {
-           
+            logger.LogDebug("get by Id method executing...");
 
-            var genre= repository.GetGenreById(Id);
+
+            var genre = repository.GetGenreById(Id);
+
+
             if (genre == null)
             {
+                logger.LogWarning($"Genre with id {Id} not found");
+                logger.LogError("this is an error");
+                //throw new ApplicationException();
                 return NotFound();
             }
-            //return Ok(2)
-           // return 'felipe'
+           
             return genre;
         }
         [HttpPost]
-        public ActionResult Post([FromBody]Genre genre)
+        public ActionResult Post([FromBody] Genre genre)
         {
             repository.AddGenre(genre);
-            return NoContent();
+            return new CreatedAtRouteResult("getGenre", new { Id = genre.Id }, genre);
         }
         [HttpPut]
-        public ActionResult  Put([FromBody] Genre genre)
+        public ActionResult Put([FromBody] Genre genre)
         {
-          
+
             return NoContent();
         }
         [HttpDelete]
--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -1,5 +1,15 @@
 ﻿using Microsoft.OpenApi.Models;
 using MoviesAPI.Services;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft;
+using MoviesAPI.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 
 namespace MoviesAPI
 {
@@ -16,8 +26,15 @@ namespace MoviesAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(MyExceptionFilter));
+            });
+            services.AddResponseCaching();
             services.AddSingleton<IRepository, InMemoryRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+            services.AddTransient<MyActionFilter>();
+
 
             services.AddSwaggerGen(c =>
             {
@@ -25,9 +42,42 @@ namespace MoviesAPI
             });
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+
+            app.Use(async (context, next) =>
+            {
+                using (var swapStream = new MemoryStream())
+                {
+                    var originalResponseBody = context.Response.Body;
+                    context.Response.Body = swapStream;
+
+                    await next.Invoke();
+
+                    swapStream.Seek(0, SeekOrigin.Begin);
+                    string responseBody = new StreamReader(swapStream).ReadToEnd();
+                    swapStream.Seek(0, SeekOrigin.Begin);
+
+                    await swapStream.CopyToAsync(originalResponseBody);
+                    context.Response.Body = originalResponseBody;
+
+                    logger.LogInformation(responseBody);
+
+
+                }
+            });
+
+            app.Map("/map1", (app) =>
+            {
+                app.Run(async context =>
+           {
+               await context.Response.WriteAsync("Im short-circuiting the pipeline");
+           });
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -38,6 +88,10 @@ namespace MoviesAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseResponseCaching();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
